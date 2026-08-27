70bf239bb2ab4c593c2b6f08bd040551d6620815
anton-evg <ab.an.evg@gmail.com>
2026-08-27 20:56:44 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,26 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+function makeStylesheetNonBlocking() {
+  return {
+    name: "stylesheet-non-blocking",
+    transformIndexHtml: {
+      order: "post",
+      handler(html) {
+        return html.replace(/<link rel="stylesheet"([^>]*?)>/g, (tag, attributes) => {
+          if (!attributes.includes("href=")) {
+            return tag;
+          }
+
+          return `<link rel="preload" as="style"${attributes} onload="this.onload=null;this.rel='stylesheet'"><noscript><link rel="stylesheet"${attributes}></noscript>`;
+        });
+      },
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), makeStylesheetNonBlocking()],
   build: {
     rollupOptions: {
       input: {
